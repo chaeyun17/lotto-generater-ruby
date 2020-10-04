464ea96a426329541f31d41bfc21408f3f9fe1ae
Chaeyun Lee <chaeyunn@iochord.com>
2020-10-04 18:18:12 +0900

    Add 로또 생성 페이지 구현

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,26 @@
 class WelcomeController < ApplicationController
   def index
+    if params.has_key?(:amount) and !params[:amount].empty?
+      amount = params.require(:amount).to_i
+    else
+      amount = 5
+    end
+    
+    @lotto_groupOfNumbers = generate_lotto(amount)
+    @amount = amount
+  end
+
+  private
+  def generate_lotto(amount)
+    groupOfNumbers = []
+    for i in (1..amount) do
+      numbers = []
+      for num in (0..5) do
+        numbers[num] = rand(1..45)
+      end
+      numbers = numbers.sort()
+      groupOfNumbers[i-1] = numbers
+    end
+    return groupOfNumbers
   end
 end
