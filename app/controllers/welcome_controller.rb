822565a3578d67ed47e5beb1da025895f4c19e4f
Chaeyun Lee <hopefulyun@gmail.com>
2021-03-01 17:14:43 +0900

    Contoller 리팩토링, Path 수정

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,37 +1,4 @@
-require 'set'
-
 class WelcomeController < ApplicationController
   def index
-    if params.has_key?(:amount) and !params[:amount].empty?
-      amount = params.require(:amount).to_i
-    else
-      amount = 5
-    end
-    
-    @lotto_groupOfNumbers = generate_lotto(amount)
-    @amount = amount
   end
-
-  private
-  def generate_lotto(amount)
-    groupOfNumbers = []
-    for i in (1..amount) do
-      groupOfNumbers[i-1] = generate_lotto_number()
-    end
-    return groupOfNumbers
-  end
-
-  private
-  def generate_lotto_number()
-    picks = Set.new
-    for i in (1..6) do
-      randomNum = rand(1..45)
-      while picks.member?(randomNum) do 
-        randomNum = rand(1..45)
-      end
-      picks.add(randomNum)
-    end
-    return picks
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-  get 'lotto', to: 'lotto#index'
+  root 'lotto#lotto_numbers'
+  
+  get 'welcome/index'
+  get 'lotto', to: 'lotto#lotto_numbers'  
 end
